cc7bb1f6b7090293490bd2f0ea4a1c1b8e06ea34
YasserDalal <170532228+YasserDalal@users.noreply.github.com>
2025-03-30 21:33:01 +0300

    Create separate repo for organizing

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+tailwind.config = {
+  theme: {
+    extend: {
+      screens: {
+        'md': '768px', // Default
+        'max-md': '500px', // Custom breakpoint
+      },
+    },
+  },
+  plugins: [],
+}
